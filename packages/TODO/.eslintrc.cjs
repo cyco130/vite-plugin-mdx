12bbe02f7c820bf0aabcc17486e175493ced67b8
cyco130 <cyco130@gmail.com>
2022-06-05 12:20:05 +0300

    chore: Set up monorepo

---
+++ packages/TODO/.eslintrc.cjs
@@ -0,0 +1,8 @@
+require("@cyco130/eslint-config/patch");
+
+module.exports = {
+  root: true,
+  extends: ["@cyco130/eslint-config/node"],
+  parserOptions: { tsconfigRootDir: __dirname },
+  rules: {},
+};
---
+++ packages/TODO/lint-staged.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  "**/*.ts?(x)": [
+    () => "tsc -p tsconfig.json --noEmit",
+    "eslint --max-warnings 0 --ignore-pattern dist",
+    "vitest related --run",
+  ],
+};
