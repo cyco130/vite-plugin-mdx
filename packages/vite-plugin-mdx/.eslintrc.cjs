81c2b296512aa2db865462eb7e760d1576b2e20d
Fatih Aygün <cyco130@gmail.com>
2024-11-02 08:56:24 +0000

    chore: update dependencies (#39)

--- packages/vite-plugin-mdx/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-require("@cyco130/eslint-config/patch");
-
-module.exports = {
-  root: true,
-  extends: ["@cyco130/eslint-config/node"],
-  ignorePatterns: ["node_modules", "dist", "**/*.cjs"],
-  parserOptions: { project: [__dirname + "/tsconfig.json"] },
-  rules: {},
-};
