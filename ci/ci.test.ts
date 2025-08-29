7f7f83161fba5372993212ab1c269ea9852a553a
Fatih Aygün <cyco130@gmail.com>
2025-08-29 17:23:45 +0100

    chore: update workflow deps

--- ci/ci.test.ts
+++ ci/ci.test.ts
@@ -13,6 +13,7 @@ const psTree = promisify(psTreeCb);
 const TEST_HOST = "http://localhost:5174";
 
 const browser = await puppeteer.launch({
+  headless: true,
   defaultViewport: { width: 1200, height: 800 },
 });
 
